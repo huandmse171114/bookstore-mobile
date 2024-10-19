e41f3866d4afde0c6e52898de9e22ace86d6b7fa
SeY1n <nhatlmse171573@fpt.edu.vn>
2024-10-19 14:28:20 +0700

    Welcome layout

---
+++ bookstore/app/src/main/java/com/bookstore/model/SliderViewAdapter.java
@@ -0,0 +1,104 @@
+package com.bookstore.model;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.bookstore.MainActivity;
+import com.bookstore.R;
+import com.bookstore.databinding.SlideScreenBinding;
+import com.bookstore.view.SliderActivity;
+
+
+public class SliderViewAdapter extends PagerAdapter {
+
+    Context mContext;
+    private SlideScreenBinding binding;
+
+
+    public SliderViewAdapter(Context mContext) {
+        this.mContext = mContext;
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        binding = SlideScreenBinding.inflate(LayoutInflater.from(mContext), container, false);
+
+        binding.btnGetStarted.setOnClickListener(v -> {
+            //open main activity
+            Intent intent = new Intent(mContext, MainActivity.class);
+            intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+        });
+        binding.next.setOnClickListener(v -> {
+            //forwards to the next screen
+            SliderActivity.screenPager.setCurrentItem(position + 1);
+        });
+        binding.back.setOnClickListener(v -> {
+            //backwards to the next screen
+            SliderActivity.screenPager.setCurrentItem(position - 1);
+        });
+        switch (position){
+            case 0:
+                binding.logo.setImageResource(R.drawable.bookslider);
+                binding.ind1.setImageResource(R.drawable.selected);
+                binding.ind2.setImageResource(R.drawable.unselected);
+                binding.ind3.setImageResource(R.drawable.unselected);
+
+                binding.title.setText("Variety of Books");
+                binding.desc.setText("We have a wide range of books for you to choose from.");
+                binding.back.setVisibility(View.GONE);
+                binding.next.setVisibility(View.VISIBLE);
+                break;
+             case 1:
+                 binding.logo.setImageResource(R.drawable.shipper);
+                 binding.ind1.setImageResource(R.drawable.unselected);
+                 binding.ind2.setImageResource(R.drawable.selected);
+                 binding.ind3.setImageResource(R.drawable.unselected);
+
+                 binding.title.setText("Delivery faster");
+                 binding.desc.setText("We deliver your order as soon as possible.");
+                 binding.back.setVisibility(View.VISIBLE);
+                 binding.next.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                binding.logo.setImageResource(R.drawable.payment);
+                binding.ind1.setImageResource(R.drawable.unselected);
+                binding.ind2.setImageResource(R.drawable.unselected);
+                binding.ind3.setImageResource(R.drawable.selected);
+
+                binding.title.setText("Easy Payment");
+                binding.desc.setText("You can pay easily with various payment methods.");
+                binding.next.setVisibility(View.GONE);
+                binding.back.setVisibility(View.VISIBLE);
+                break;
+            }
+        container.addView(binding.getRoot());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
---
+++ bookstore/app/src/main/java/com/bookstore/view/OnboardingActivity.java
@@ -0,0 +1,53 @@
+package com.bookstore.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bookstore.databinding.OnboardingLayoutBinding;
+
+public class OnboardingActivity extends AppCompatActivity {
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        // Khởi tạo binding cho layout random_main
+        com.bookstore.databinding.OnboardingLayoutBinding binding = OnboardingLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // Đặt padding cho view
+        ViewCompat.setOnApplyWindowInsetsListener(binding.onboarding, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Animation
+        binding.img.animate().translationY(-2500).setDuration(1000).setStartDelay(4000);
+        binding.logo.animate().translationY(2400).setDuration(1000).setStartDelay(4000);
+        binding.appName.animate().translationY(2400).setDuration(1000).setStartDelay(4000);
+        binding.lottie.animate().translationY(2400).setDuration(1000).setStartDelay(4000);
+
+        // forward slider screen
+        long totalDuration = 5300;
+        new Handler(Looper.getMainLooper()).postDelayed(this::navigateToSliderActivity, totalDuration);
+     }
+    private void navigateToSliderActivity() {
+        Intent intent = new Intent(this, SliderActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent); // start the activity
+        finish();
+    }
+}
+
+
